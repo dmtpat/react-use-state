ce039d7db8da364709129bbf2c305f59426e4f78
Pat <dmtpatricica@email.com>
2026-01-18 23:49:09 +0100

    deactivate button

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <h1 className='fw-bold fs-1' >Learn Web Development</h1>
       <div className="container mt-5">
         {languages.map((language, index) => (
-          <button key={index} className={`btn btn-primary m-2 ${selected_id == index ? "selected" : ""}`} onClick={() => setSelected_id(index)}>{language.title}</button>
+          <button key={index} className={`btn btn-primary m-2 ${selected_id == index ? "selected" : ""}`} onClick={() => { selected_id == index ? setSelected_id(-1) : setSelected_id(index) }}>{language.title}</button>
         ))}
         {selected_id >= 0 && <Card id={languages[selected_id].id} title={languages[selected_id].title} description={languages[selected_id].description} />
         }
