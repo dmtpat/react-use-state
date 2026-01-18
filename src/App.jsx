c277d8c4dfcae5a157bce693099ced115ae980ac
Pat <dmtpatricica@email.com>
2026-01-18 23:15:28 +0100

    base ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import languages from './data/languages'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,13 @@ function App() {
   return (
     <>
       <h1 className='fw-bold fs-1' >Learn Web Development</h1>
-      <div className="container mt-5"></div>
+      <div className="container mt-5">
+        <button className='btn btn-primary'>{languages[0].title}</button>
+        <div key={languages[0].id} className="card p-1 mt-4">
+          <h2>{languages[0].title}</h2>
+          <p>{languages[0].description}</p>
+        </div>
+      </div>
     </>
   )
 }
