36785401a66ca2bdcc9e580cb48faa1516ccb711
Pat <dmtpatricica@email.com>
2026-01-18 23:30:12 +0100

    Active Button

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <h1 className='fw-bold fs-1' >Learn Web Development</h1>
       <div className="container mt-5">
         {languages.map((language, index) => (
-          <button key={index} className='btn btn-primary m-2' onClick={() => setSelected_id(index)}>{language.title}</button>
+          <button key={index} className={`btn btn-primary m-2 ${selected_id == index ? "selected" : ""}`} onClick={() => setSelected_id(index)}>{language.title}</button>
         ))}
         <div key={languages[selected_id].id} className="card p-1 mt-4">
           <h2>{languages[selected_id].title}</h2>
