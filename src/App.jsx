84d8359a2ec1aa685450829886d00d297a21c74f
Pat <dmtpatricica@email.com>
2026-01-18 23:25:25 +0100

    button selected card

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import './App.css'
 import languages from './data/languages'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selected_id, setSelected_id] = useState(0)
 
   return (
     <>
       <h1 className='fw-bold fs-1' >Learn Web Development</h1>
       <div className="container mt-5">
         {languages.map((language, index) => (
-          <button key={index} className='btn btn-primary m-2'>{language.title}</button>
+          <button key={index} className='btn btn-primary m-2' onClick={() => setSelected_id(index)}>{language.title}</button>
         ))}
-        <div key={languages[0].id} className="card p-1 mt-4">
-          <h2>{languages[0].title}</h2>
-          <p>{languages[0].description}</p>
+        <div key={languages[selected_id].id} className="card p-1 mt-4">
+          <h2>{languages[selected_id].title}</h2>
+          <p>{languages[selected_id].description}</p>
         </div>
       </div>
     </>
